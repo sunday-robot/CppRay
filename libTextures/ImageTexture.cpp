1dd535857a1278e8d8351e34c11294e5b61e7153
sunday robot <sgx03150@nifty.com>
2023-12-23 15:12:11 +0900

    std::clampを使うように修正してみた。

--- libTextures/ImageTexture.cpp
+++ libTextures/ImageTexture.cpp
@@ -1,4 +1,6 @@
 #include "ImageTexture.h"
+
+#include <algorithm>
 #include "loadBmp.h"
 
 ImageTexture::Image ImageTexture::loadBmpAsImage(std::string filePath) {
@@ -9,22 +11,14 @@ ImageTexture::Image ImageTexture::loadBmpAsImage(std::string filePath) {
 	return Image{ width, height,data };
 }
 
-static inline double clamp01(double value) {
-	if (value < 0)
-		return 0;
-	if (value > 1)
-		return 1;
-	return value;
-}
-
 Color ImageTexture::value(double u, double v, const Vec3& p) const {
 	// If we have no texture data, then return solid cyan as a debugging aid.
 	if (_data == 0)
 		return Color::cyan;
 
 	// Clamp input texture coordinates to [0,1] x [1,0]
-	u = clamp01(u);
-	v = 1 - clamp01(v);  // Flip V to image coordinates
+	u = std::clamp(u, 0.0, 1.0);
+	v = 1 - std::clamp(v, 0.0, 1.0);  // Flip V to image coordinates
 
 	auto x = std::min((int)(u * _width), _width - 1);
 	auto y = std::min((int)(v * _height), _height - 1);
